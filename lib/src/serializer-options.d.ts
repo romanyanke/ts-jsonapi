1beca930aa7fb7c9d6bfaf88a913307b0d997e46
romanyanke <roman@yanke.ru>
2018-10-19 16:23:23 +0300

    add compiled lib/ files with circular reference fix

---
+++ lib/src/serializer-options.d.ts
@@ -0,0 +1,20 @@
+import { Links, caseOptions, RelationshipMeta } from './types';
+export interface SerializerOptions {
+    [index: string]: any;
+    id: string;
+    attributes: Array<string>;
+    topLevelLinks?: Links;
+    keyForAttribute?: (attribute: string) => string | caseOptions;
+    ref?: string | boolean | Function;
+    typeForAttribute?: (attribute: any, user: any) => any;
+    nullIfMissing?: boolean;
+    pluralizeType?: boolean;
+    ignoreRelationshipData?: boolean;
+    relationshipLinks?: Links;
+    relationshipMeta?: RelationshipMeta;
+    dataLinks?: Links;
+    included?: boolean;
+    includedLinks?: Links;
+    embed?: boolean;
+    meta?: any;
+}
